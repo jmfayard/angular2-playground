83046a3b1960a41d1ae02a83707dd30958b656c9
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2017-02-22 12:06:08 +0100

    404

--- src/app/404/page-not-found.component.ts
+++ src/app/404/page-not-found.component.ts
@@ -3,9 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   template: `
-    <p>
-      page-not-found Works!
-    </p>
+<div data-name="component">
+  <section class="vh-100 bg-washed-blue baskerville">
+  <header class="tc ph5 lh-copy">
+      <h1 class="f1 f-headline-l code mb3 fw9 dib tracked-tight light-purple">404</h1>
+      <h2 class="tc f1-l fw1">Sorry, we can't find the page you are looking for.</h2>
+  </header>
+ 
+</section>
+
+  </div>
   `,
   styles: []
 })
